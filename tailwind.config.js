abf679064dff086b72ed17eb25e31ff007736784
Usman Qazi <usmanqazi@MacBook-Pro.local>
2021-07-24 16:28:55 -0700

    Fixed footer formatting and link box formatting

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '160': '40rem'
+      }
+    },
   },
   variants: {
     extend: {},
